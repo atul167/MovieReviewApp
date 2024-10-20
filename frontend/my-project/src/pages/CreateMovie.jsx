1b0d881a8346fd41daddc4d8a6cd128c30f4485c
Atul Dwivedi <atuldwivedi1628@gmail.com>
2024-10-20 17:19:19 +0530

    Added create functionality

--- frontend/my-project/src/pages/CreateMovie.jsx
+++ frontend/my-project/src/pages/CreateMovie.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import { useState,useEffect } from 'react'
 import BackButton from '../components/BackButton'
 import { useNavigate } from 'react-router-dom'
